c8390e9629654efb3826b8124c2807ebae931429
Justas Masiulis <Justas.masiulis@yahoo.com>
2020-10-24 01:15:15 +0300

    Compilation speed optimizations

--- include/xorstr.hpp
+++ include/xorstr.hpp
@@ -21,12 +21,9 @@
 #include <cstdint>
 #include <cstddef>
 #include <utility>
+#include <type_traits>
 
-#define xorstr(str)                                             \
-    ::jm::make_xorstr(                                          \
-        []() { return str; },                                   \
-        std::make_index_sequence<sizeof(str) / sizeof(*str)>{}, \
-        std::make_index_sequence<::jm::detail::_buffer_size<sizeof(str)>()>{})
+#define xorstr(str) ::jm::xor_string{[]() { return str; }, std::integral_constant<std::size_t, sizeof(str) / sizeof(*str)>{}, std::make_index_sequence<::jm::detail::_buffer_size<sizeof(str)>()>{} }
 #define xorstr_(str) xorstr(str).crypt_get()
 
 #ifdef _MSC_VER
@@ -35,57 +32,21 @@
 #define XORSTR_FORCEINLINE __attribute__((always_inline)) inline
 #endif
 
+#if defined(__clang__) || defined(__GNUC__)
+#define JM_XORSTR_LOAD_FROM_REG(x) ::jm::detail::load_from_reg(x)
+#else
+#define JM_XORSTR_LOAD_FROM_REG(x) (x)
+#endif
+
 namespace jm {
 
     namespace detail {
-
-        template<std::size_t S>
-        struct unsigned_;
-
-        template<>
-        struct unsigned_<1> {
-            using type = std::uint8_t;
-        };
-        template<>
-        struct unsigned_<2> {
-            using type = std::uint16_t;
-        };
-        template<>
-        struct unsigned_<4> {
-            using type = std::uint32_t;
-        };
-
-        template<auto C, auto...>
-        struct pack_value_type {
-            using type = decltype(C);
-        };
 
         template<std::size_t Size>
         XORSTR_FORCEINLINE constexpr std::size_t _buffer_size()
         {
             return ((Size / 16) + (Size % 16 != 0)) * 2;
         }
-
-        template<auto... Cs>
-        struct tstring_ {
-            using value_type                  = typename pack_value_type<Cs...>::type;
-            constexpr static std::size_t size = sizeof...(Cs);
-            constexpr static value_type  str[size] = { Cs... };
-
-            constexpr static std::size_t buffer_size = _buffer_size<sizeof(str)>();
-            constexpr static std::size_t buffer_align =
-#ifndef JM_XORSTR_DISABLE_AVX_INTRINSICS
-                ((sizeof(str) > 16) ? 32 : 16);
-#else
-                16;
-#endif
-        };
-
-        template<std::size_t I, std::uint64_t K>
-        struct _ki {
-            constexpr static std::size_t   idx = I;
-            constexpr static std::uint64_t key = K;
-        };
 
         template<std::uint32_t Seed>
         XORSTR_FORCEINLINE constexpr std::uint32_t key4() noexcept
@@ -105,18 +66,18 @@ namespace jm {
         }
 
         // loads up to 8 characters of string into uint64 and xors it with the key
-        template<class T>
+        template<std::size_t N, class CharT>
         XORSTR_FORCEINLINE constexpr std::uint64_t
-        load_xored_str8(std::uint64_t key, std::size_t idx) noexcept
+        load_xored_str8(std::uint64_t key, std::size_t idx, const CharT* str) noexcept
         {
-            using cast_type = typename unsigned_<sizeof(typename T::value_type)>::type;
-            constexpr auto value_size = sizeof(typename T::value_type);
+            using cast_type = typename std::make_unsigned<CharT>::type;
+            constexpr auto value_size = sizeof(CharT);
             constexpr auto idx_offset = 8 / value_size;
 
             std::uint64_t value = key;
-            for(std::size_t i = 0; i < idx_offset && i + idx * idx_offset < T::size; ++i)
+            for(std::size_t i = 0; i < idx_offset && i + idx * idx_offset < N; ++i)
                 value ^=
-                    (std::uint64_t{ static_cast<cast_type>(T::str[i + idx * idx_offset]) }
+                    (std::uint64_t{ static_cast<cast_type>(str[i + idx * idx_offset]) }
                      << ((i % idx_offset) * 8 * value_size));
 
             return value;
@@ -131,25 +92,6 @@ namespace jm {
             return value;
         }
 
-        XORSTR_FORCEINLINE void xor128(std::uint64_t*       value,
-                                       const std::uint64_t* key) noexcept
-        {
-            _mm_store_si128(
-                reinterpret_cast<__m128i*>(value),
-                _mm_xor_si128(_mm_load_si128(reinterpret_cast<const __m128i*>(value)),
-                              _mm_load_si128(reinterpret_cast<const __m128i*>(key))));
-        }
-
-        XORSTR_FORCEINLINE void xor256(std::uint64_t*       value,
-                                       const std::uint64_t* key) noexcept
-        {
-            _mm256_store_si256(
-                reinterpret_cast<__m256i*>(value),
-                _mm256_xor_si256(
-                    _mm256_load_si256(reinterpret_cast<const __m256i*>(value)),
-                    _mm256_load_si256(reinterpret_cast<const __m256i*>(key))));
-        }
-
         template<std::uint64_t V>
         struct uint64_v {
             constexpr static std::uint64_t value = V;
@@ -157,59 +99,63 @@ namespace jm {
 
     } // namespace detail
 
-    template<class T, class... Keys>
-    class xor_string {
-        alignas(T::buffer_align) std::uint64_t _storage[T::buffer_size];
+    template<class CharT, std::size_t Size, class Keys, class Indices>
+    class xor_string;
 
-        template<std::size_t... Idxs>
-        XORSTR_FORCEINLINE void _crypt_256(const std::uint64_t* keys,
-                                           std::index_sequence<Idxs...>) noexcept
-        {
-            (detail::xor256(_storage + Idxs * 4, keys + Idxs * 4), ...);
-        }
+    template<class CharT, std::size_t Size, std::uint64_t... Keys, std::size_t... Indices>
+    class xor_string<CharT, Size, std::integer_sequence<std::uint64_t, Keys...>, std::index_sequence<Indices...>> {
+#ifndef JM_XORSTR_DISABLE_AVX_INTRINSICS
+        constexpr static inline std::uint64_t alignment = ((Size > 16) ? 32 : 16);    
+#else
+        constexpr static inline std::uint64_t alignment = 16;
+#endif
 
-        template<std::size_t... Idxs>
-        XORSTR_FORCEINLINE void _crypt_128(const std::uint64_t* keys,
-                                           std::index_sequence<Idxs...>) noexcept
-        {
-            (detail::xor128(_storage + Idxs * 2, keys + Idxs * 2), ...);
-        }
+        alignas(alignment) std::uint64_t _storage[sizeof...(Keys)];
 
     public:
-        using value_type    = typename T::value_type;
+        using value_type    = CharT;
         using size_type     = std::size_t;
-        using pointer       = value_type*;
-        using const_pointer = const value_type*;
+        using pointer       = CharT*;
+        using const_pointer = const CharT*;
 
-        XORSTR_FORCEINLINE xor_string() noexcept
-            : _storage{ detail::load_from_reg(detail::uint64_v<detail::load_xored_str8<T>(
-                                                  Keys::key, Keys::idx)>::value)... }
+        template<class L>
+        XORSTR_FORCEINLINE xor_string(L l, std::integral_constant<std::size_t, Size>, std::index_sequence<Indices...>) noexcept
+            : _storage{ JM_XORSTR_LOAD_FROM_REG(detail::uint64_v<detail::load_xored_str8<Size>(Keys, Indices, l())>::value)... }
         {}
 
         XORSTR_FORCEINLINE constexpr size_type size() const noexcept
         {
-            return T::size - 1;
+            return Size - 1;
         }
 
         XORSTR_FORCEINLINE void crypt() noexcept
         {
 #if defined(__clang__)
-            alignas(T::buffer_align)
-                std::uint64_t arr[sizeof...(Keys)]{ detail::load_from_reg(Keys::key)... };
+            alignas(alignment)
+                std::uint64_t arr[]{ JM_XORSTR_LOAD_FROM_REG(Keys)... };
             std::uint64_t*    keys =
-                (std::uint64_t*)detail::load_from_reg((std::uint64_t)arr);
+                (std::uint64_t*)JM_XORSTR_LOAD_FROM_REG((std::uint64_t)arr);
 #else
-            alignas(T::buffer_align) std::uint64_t keys[sizeof...(Keys)]{
-                detail::load_from_reg(Keys::key)...
-            };
+            alignas(alignment) std::uint64_t keys[]{ JM_XORSTR_LOAD_FROM_REG(Keys)... };
 #endif
 
 #ifndef JM_XORSTR_DISABLE_AVX_INTRINSICS
-            _crypt_256(keys, std::make_index_sequence<T::buffer_size / 4>{});
-            if constexpr(T::buffer_size % 4 != 0)
-                _crypt_128(keys, std::index_sequence<T::buffer_size / 2 - 1>{});
+            ((Indices >= sizeof(_storage) / 32 ? static_cast<void>(0) : _mm256_store_si256(
+                reinterpret_cast<__m256i*>(_storage) + Indices,
+                _mm256_xor_si256(
+                    _mm256_load_si256(reinterpret_cast<const __m256i*>(_storage) + Indices),
+                    _mm256_load_si256(reinterpret_cast<const __m256i*>(keys) + Indices)))), ...);
+
+            if constexpr(sizeof(_storage) % 32 != 0)
+                _mm_store_si128(
+                    reinterpret_cast<__m128i*>(_storage + sizeof...(Keys) - 2),
+                    _mm_xor_si128(_mm_load_si128(reinterpret_cast<const __m128i*>(_storage + sizeof...(Keys) - 2)),
+                                _mm_load_si128(reinterpret_cast<const __m128i*>(keys + sizeof...(Keys) - 2))));
 #else
-            _crypt_128(keys, std::make_index_sequence<T::buffer_size / 2>{});
+        ((Indices >= sizeof(_storage) / 16 ? static_cast<void>(0) : _mm_store_si128(
+            reinterpret_cast<__m128i*>(_storage) + Indices,
+            _mm_xor_si128(_mm_load_si128(reinterpret_cast<const __m128i*>(_storage) + Indices),
+                            _mm_load_si128(reinterpret_cast<const __m128i*>(keys) + Indices)))), ...);
 #endif
         }
 
@@ -230,15 +176,12 @@ namespace jm {
         }
     };
 
-    template<class Tstr, std::size_t... StringIndices, std::size_t... KeyIndices>
-    XORSTR_FORCEINLINE constexpr auto
-    make_xorstr(Tstr str_lambda,
-                std::index_sequence<StringIndices...>,
-                std::index_sequence<KeyIndices...>) noexcept
-    {
-        return xor_string<detail::tstring_<str_lambda()[StringIndices]...>,
-                          detail::_ki<KeyIndices, detail::key8<KeyIndices>()>...>{};
-    }
+    template<class L, std::size_t Size, std::size_t... Indices>
+    xor_string(L l, std::integral_constant<std::size_t, Size>, std::index_sequence<Indices...>) -> xor_string<
+                std::remove_reference_t<decltype(l()[0])>,
+                Size,
+                std::integer_sequence<std::uint64_t, detail::key8<Indices>()...>,
+                std::index_sequence<Indices...>>;
 
 } // namespace jm
 
